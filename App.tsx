ac3e3bcd45d3eeb8ff5b299d96fd3df71d1c433e
LG-BaoPTIT <bao2002pytn@gmail.com>
2024-03-13 21:57:58 +0700

    first commit

---
+++ App.tsx
@@ -0,0 +1,78 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type { PropsWithChildren } from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+
+
+  return (
+    <SafeAreaView style={{ backgroundColor: 'orange' }}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+
+      />
+      <View style={{ borderWidth: 2, width: '100%', height: "100%", }}>
+        <View style={{ height: '20%', backgroundColor: 'pink' }}>
+          <View style={{ width:'50%',height: '100%', backgroundColor: 'red' }}>
+
+
+
+          </View>
+
+
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
